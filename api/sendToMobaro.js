26fce2a101ff81bf41fcda47447e6bb6f0ec8b7d
lbmobaro <123754583+lbmobaro@users.noreply.github.com>
2023-10-18 13:13:28 -0400

    Update sendToMobaro.js

--- api/sendToMobaro.js
+++ api/sendToMobaro.js
@@ -12,7 +12,6 @@ const upload = multer({ storage });
 
 app.post("/api/sendToMobaro", upload.single("attachment"), async (req, res) => {
     const projectData = {
-        name: req.body.name,
         description: req.body.description,
         start: req.body.startDate,
         end: req.body.completionDate,
