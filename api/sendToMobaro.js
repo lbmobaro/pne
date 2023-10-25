92d4e14cb98aed36bdc78a19a8445b1198749631
lbmobaro <123754583+lbmobaro@users.noreply.github.com>
2023-10-25 12:57:56 -0400

    Update sendToMobaro.js

--- api/sendToMobaro.js
+++ api/sendToMobaro.js
@@ -1,59 +1,75 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
-const app = express();
+document.getElementById("projectForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-app.use(bodyParser.json());
+  // Generate the formatted description based on form input values
+  const formattedDescription = generateFormattedDescription();
 
-app.post("/api/sendToMobaro", async (req, res) => {
-    try {
-        const projectData = {
-            name: req.body.description,
-            description: req.body.formattedDescription, // Use the user-entered description as is
-            assignees: "users/112899-C",
-            start: req.body.startDate,
-            end: req.body.completionDate,
-            target: "locations/109477-A",
-            // You can handle file uploads separately if needed
-            // attachments: req.file ? [req.file.buffer.toString("base64")] : [],
-        };
+  // Create an object to hold the form data
+  const formData = {
+    name: document.getElementById("name").value,
+    department: document.getElementById("department").value,
+    attachments: document.getElementById("attachments").files[0], // Handle file uploads separately if needed
+    siteContact: document.getElementById("siteContact").value,
+    startDate: document.getElementById("startDate").value,
+    completionDate: document.getElementById("completionDate").value,
+    glCode: document.getElementById("glCode").value,
+    costCenterCode: document.getElementById("costCenterCode").value,
+    costUnitCode: document.getElementById("costUnitCode").value,
+    projectCode: document.getElementById("projectCode").value,
+    projectBudget: document.getElementById("projectBudget").value,
+    formattedDescription: formattedDescription,
+  };
 
-        console.log("Sending projectData to Mobaro API:", projectData);
+  // Convert the formData object to a JSON string
+  const jsonData = JSON.stringify(formData);
 
-        // Prepare headers for the Mobaro API request
-        const headers = {
-            "x-api-key": process.env.MOBARO_API_KEY, // Use your API key stored in environment variables
-            "Content-Type": "application/json",
-        };
-
-        // Perform the POST request to the Mobaro API
-        const response = await fetch("https://app.mobaro.com/api/customers/assignments", {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(projectData),
-        });
-
-        const responseBody = await response.text(); // Read the response as text
-
-        if (response.ok) {
-            // Success response from Mobaro API
-            res.json({ message: "Project data sent to Mobaro successfully!" });
-        } else {
-            // Error response from Mobaro API
-            console.error(`Error response from Mobaro API: ${response.status} ${response.statusText}`);
-            console.error(`Response Body: ${responseBody}`);
-
-            // Send an error response to the client
-            res.status(response.status).json({ error: "Error sending project data to Mobaro." });
-        }
-    } catch (error) {
-        // Handle any network or request errors
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  // Send the JSON data to the server
+  fetch("/api/sendToMobaro", {
+    method: "POST",
+    body: jsonData,
+    headers: {
+      "Content-Type": "application/json", // Set the content type to JSON
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the response from the server (e.g., show a success message)
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error(error);
+    });
 });
 
-const port = process.env.PORT || 3000; // Use the port defined in environment variables or default to 3000
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+function generateFormattedDescription() {
+  // Get values from form fields
+  const name = document.getElementById("name").value;
+  const department = document.getElementById("department").value;
+  const userDescription = document.getElementById("description").value; // User-entered description
+  const siteContact = document.getElementById("siteContact").value;
+  const startDate = document.getElementById("startDate").value;
+  const completionDate = document.getElementById("completionDate").value;
+  const glCode = document.getElementById("glCode").value;
+  const costCenterCode = document.getElementById("costCenterCode").value;
+  const costUnitCode = document.getElementById("costUnitCode").value;
+  const projectCode = document.getElementById("projectCode").value;
+  const projectBudget = document.getElementById("projectBudget").value;
+
+  // Create the formatted description with bold titles and input values
+  const formattedDescription = {
+    Name: name,
+    Department: department,
+    "User Description": userDescription,
+    "Site Contact": siteContact,
+    "Start Date": startDate,
+    "End Date": completionDate,
+    "GL Code": glCode,
+    "Cost Center Code": costCenterCode,
+    "Cost Unit Code": costUnitCode,
+    "Project Code": projectCode,
+    "Project Budget": projectBudget,
+  };
+
+  return formattedDescription;
+}
