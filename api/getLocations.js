170abbb1b1d35e60515f21e160437b9c6b5cac17
lbmobaro <123754583+lbmobaro@users.noreply.github.com>
2023-10-25 16:46:37 -0400

    Update getLocations.js

--- api/getLocations.js
+++ api/getLocations.js
@@ -1,51 +1,42 @@
 const fetch = require("node-fetch");
 
-module.exports = async (req, res) => {
-    try {
-        const headers = {
-            "x-api-key": process.env.MOBARO_API_KEY
-        };
+exports.handler = async (event, context) => {
+  try {
+    const headers = {
+      "x-api-key": process.env.MOBARO_API_KEY,
+    };
 
-        const allLocations = [];
+    const response = await fetch("https://app.mobaro.com/api/customers/locations", {
+      method: "GET",
+      headers: headers,
+    });
 
-        let offset = 0;
-        let total = 0;
-        let amount = 0;
-
-        do {
-            // Fetch location data from Mobaro API with the current offset
-            const response = await fetch(`https://app.mobaro.com/api/customers/locations?offset=${offset}`, {
-                method: "GET",
-                headers: headers,
-            });
-
-            if (!response.ok) {
-                throw new Error(`Error fetching location data: ${response.statusText}`);
-            }
-
-            const responseData = await response.json();
-
-            if (responseData.items) {
-                // Extract and push `id` and `name` properties from each item to allLocations
-                responseData.items.forEach((item) => {
-                    allLocations.push({
-                        id: item.id,
-                        name: item.name,
-                    });
-                });
-            }
-
-            // Update pagination variables
-            total = responseData.total;
-            amount = responseData.amount;
-            offset += amount;
-        } while (offset < total);
-
-        // Sort the locations alphabetically by the `name` property
-        allLocations.sort((a, b) => a.name.localeCompare(b.name));
-
-        res.status(200).json(allLocations);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (!response.ok) {
+      throw new Error(`Error fetching location data: ${response.statusText}`);
     }
+
+    const locationsData = await response.json();
+
+    // Create a mapping between name and ID
+    const nameToIdMap = {};
+    locationsData.items.forEach((location) => {
+      nameToIdMap[location.name] = location.id;
+    });
+
+    // Retrieve the selected location name from the request (modify as needed)
+    const selectedLocationName = event.queryStringParameters.target;
+
+    // Use the name-to-ID mapping to get the corresponding ID
+    const selectedLocationId = nameToIdMap[selectedLocationName];
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ target: selectedLocationId }), // Return the ID as the target
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
