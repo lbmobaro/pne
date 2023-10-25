e95e9337e50380356f3545c304ba2aff8f251e38
lbmobaro <123754583+lbmobaro@users.noreply.github.com>
2023-10-25 15:07:23 -0400

    Update getLocations.js

--- api/getLocations.js
+++ api/getLocations.js
@@ -6,20 +6,44 @@ exports.handler = async (event, context) => {
             "x-api-key": process.env.MOBARO_API_KEY
         };
 
-        const response = await fetch("https://app.mobaro.com/api/customers/locations", {
-            method: "GET",
-            headers: headers,
-        });
+        const allLocations = [];
 
-        if (!response.ok) {
-            throw new Error(`Error fetching location data: ${response.statusText}`);
-        }
+        let offset = 0;
+        let total = 0;
+        let amount = 0;
 
-        const locationsData = await response.json();
+        do {
+            // Fetch location data from Mobaro API with the current offset
+            const response = await fetch(`https://app.mobaro.com/api/customers/locations?offset=${offset}`, {
+                method: "GET",
+                headers: headers,
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error fetching location data: ${response.statusText}`);
+            }
+
+            const responseData = await response.json();
+
+            if (responseData.items) {
+                // Extract and push `id` and `name` properties from each item to allLocations
+                responseData.items.forEach((item) => {
+                    allLocations.push({
+                        id: item.id,
+                        name: item.name,
+                    });
+                });
+            }
+
+            // Update pagination variables
+            total = responseData.total;
+            amount = responseData.amount;
+            offset += amount;
+        } while (offset < total);
 
         return {
             statusCode: 200,
-            body: JSON.stringify(locationsData),
+            body: JSON.stringify(allLocations),
         };
     } catch (error) {
         return {
