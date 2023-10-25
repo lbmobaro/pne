8a3b09eb04bea2f781512249aac17a379c59703d
lbmobaro <123754583+lbmobaro@users.noreply.github.com>
2023-10-25 13:05:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,75 @@
 document.getElementById("projectForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get values from form fields
-    const name = document.getElementById("name").value;
-    const department = document.getElementById("department").value;
-    const userDescription = document.getElementById("description").value; // Ensure the correct ID
-    const siteContact = document.getElementById("siteContact").value;
-    const startDate = document.getElementById("startDate").value;
-    const completionDate = document.getElementById("completionDate").value;
-    const glCode = document.getElementById("glCode").value;
-    const costCenterCode = document.getElementById("costCenterCode").value;
-    const costUnitCode = document.getElementById("costUnitCode").value;
-    const projectCode = document.getElementById("projectCode").value;
-    const projectBudget = document.getElementById("projectBudget").value;
+  // Generate the formatted description based on form input values
+  const formattedDescription = generateFormattedDescription();
 
-    // Generate the formatted description
-    const formattedDescription = `
-        Name: ${name}
-        Department: ${department}
-        User Description: ${userDescription}
-        Site Contact: ${siteContact}
-        Start Date: ${startDate}
-        End Date: ${completionDate}
-        GL Code: ${glCode}
-        Cost Center Code: ${costCenterCode}
-        Cost Unit Code: ${costUnitCode}
-        Project Code: ${projectCode}
-        Project Budget: ${projectBudget}
-    `;
+  // Create an object to hold the form data
+  const formData = {
+    name: document.getElementById("name").value,
+    department: document.getElementById("department").value,
+    attachments: document.getElementById("attachments").files[0], // Handle file uploads separately if needed
+    siteContact: document.getElementById("siteContact").value,
+    startDate: document.getElementById("startDate").value,
+    completionDate: document.getElementById("completionDate").value,
+    glCode: document.getElementById("glCode").value,
+    costCenterCode: document.getElementById("costCenterCode").value,
+    costUnitCode: document.getElementById("costUnitCode").value,
+    projectCode: document.getElementById("projectCode").value,
+    projectBudget: document.getElementById("projectBudget").value,
+    formattedDescription: formattedDescription,
+  };
 
-    // Create FormData and append data
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("department", department);
-    formData.append("attachments", document.getElementById("attachments").files[0]);
-    formData.append("siteContact", siteContact);
-    formData.append("startDate", startDate);
-    formData.append("completionDate", completionDate);
-    formData.append("glCode", glCode);
-    formData.append("costCenterCode", costCenterCode);
-    formData.append("costUnitCode", costUnitCode);
-    formData.append("projectCode", projectCode);
-    formData.append("projectBudget", projectBudget);
-    formData.append("formattedDescription", formattedDescription);
+  // Convert the formData object to a JSON string
+  const jsonData = JSON.stringify(formData);
 
-    // Append the file input to the FormData
-    const fileInput = document.getElementById("attachments");
-    formData.append("attachment", fileInput.files[0]);
-
-    // Make the fetch request
-    fetch("/api/sendToMobaro", {
-        method: "POST",
-        body: formData,
-    })
+  // Send the JSON data to the server
+  fetch("/api/sendToMobaro", {
+    method: "POST",
+    body: jsonData,
+    headers: {
+      "Content-Type": "application/json", // Set the content type to JSON
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
-        // Handle the response from the server (e.g., show a success message)
-        console.log(data);
+      // Handle the response from the server (e.g., show a success message)
+      console.log(data);
     })
     .catch((error) => {
-        // Handle any errors
-        console.error(error);
+      // Handle any errors
+      console.error(error);
     });
 });
+
+function generateFormattedDescription() {
+  // Get values from form fields
+  const name = document.getElementById("name").value;
+  const department = document.getElementById("department").value;
+  const userDescription = document.getElementById("description").value; // User-entered description
+  const siteContact = document.getElementById("siteContact").value;
+  const startDate = document.getElementById("startDate").value;
+  const completionDate = document.getElementById("completionDate").value;
+  const glCode = document.getElementById("glCode").value;
+  const costCenterCode = document.getElementById("costCenterCode").value;
+  const costUnitCode = document.getElementById("costUnitCode").value;
+  const projectCode = document.getElementById("projectCode").value;
+  const projectBudget = document.getElementById("projectBudget").value;
+
+  // Create the formatted description with bold titles and input values
+  const formattedDescription = {
+    Name: name,
+    Department: department,
+    "User Description": userDescription,
+    "Site Contact": siteContact,
+    "Start Date": startDate,
+    "End Date": completionDate,
+    "GL Code": glCode,
+    "Cost Center Code": costCenterCode,
+    "Cost Unit Code": costUnitCode,
+    "Project Code": projectCode,
+    "Project Budget": projectBudget,
+  };
+
+  return formattedDescription;
+}
