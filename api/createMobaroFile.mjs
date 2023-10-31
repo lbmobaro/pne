d41580b0f6a349a8a1663f59f006bd18eb292607
lbmobaro <123754583+lbmobaro@users.noreply.github.com>
2023-10-31 08:21:17 -0400

    Update createMobaroFile.mjs

--- api/createMobaroFile.mjs
+++ api/createMobaroFile.mjs
@@ -12,22 +12,32 @@ async function createMobaroFile() {
       return;
     }
 
-    // Get the file name and create a FormData object
-    const fileName = file.name;
-    const formData = new FormData();
-    formData.append('File', file, { filename: fileName, contentType: file.type });
+    // Read the file as an ArrayBuffer
+    const fileBuffer = await file.arrayBuffer();
+
+    // Generate a unique boundary string
+    const boundary = `------------------------${Math.random().toString(16)}`;
+
+    // Construct the body payload as a Buffer
+    const body = Buffer.concat([
+      Buffer.from(`--${boundary}\r\n`),
+      Buffer.from(`Content-Disposition: form-data; name="File"; filename="${file.name}"\r\n`),
+      Buffer.from('Content-Type: application/octet-stream\r\n\r\n'),
+      Buffer.from(fileBuffer),
+      Buffer.from(`\r\n--${boundary}--\r\n`),
+    ]);
 
     // Construct the headers for the request
     const headers = {
-      "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`,
-      "x-api-key": process.env.MOBARO_API_KEY,
+      'Content-Type': `multipart/form-data; boundary=${boundary}`,
+      'x-api-key': process.env.MOBARO_API_KEY,
     };
 
     // Send the request to Mobaro
     const response = await fetch('https://app.mobaro.com/api/customers/files/create', {
       method: 'POST',
       headers,
-      body: formData,
+      body,
     });
 
     if (response.ok) {
